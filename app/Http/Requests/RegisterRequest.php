fe28e25529fb727e73ee672247403fc23b1f497c
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-09-30 07:19:50 +0300

    feat: add register request

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    use ValidationErrors;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'bail|required|string',
+            'email' => 'email|string|unique:users',
+            'password' => 'required|string',
+            'enterprise_name' => 'required|string',
+            'lastname' => 'required|string',
+            'phone_number' => 'required|string'
+        ];
+    }
+}
