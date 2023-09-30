843f82f1e1b36df47f3345a86c99cc7c3b73031a
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-09-30 13:53:54 +0300

    feat : configuration stripe + add service subscription and plan

---
+++ app/Services/SubscriptionService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Services;
+
+use App\Events\Subscribed;
+use App\Models\Subscription;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class SubscriptionService
+{
+    protected $stripe;
+    public function __construct()
+    {
+        $this->stripe = new \Stripe\StripeClient(
+            env('STRIPE_SECRET')
+        );
+    }
+
+    public function subscribeUser(User $user, string $formationDuration, string $cardNumber, int $expireMonth, int $expireYear, int $cvc, int $amount): Subscription
+    {
+        return DB::transaction(function () use ($user, $formationDuration, $cardNumber, $expireMonth, $expireYear, $cvc, $amount) {
+            $subscription = new Subscription([
+                'user_id' => $user->id,
+                'formation_duration' => $formationDuration,
+                'card_number' => $cardNumber,
+            ]);
+            $token = $this->stripe->tokens->create([
+                'card' => [
+                    'number' => $cardNumber,
+                    'exp_month' => $expireMonth,
+                    'exp_year' => $expireYear,
+                    'cvc' => $cvc,
+                ]
+            ]);
+
+            $charge = $this->stripe->charges->create([
+                'amount' => $amount * 100,
+                'currency' => 'cad',
+                'source' => $token->id,
+                'description' => 'Paiement pour le pack #',
+                'transfer_group' => 'ORDER',
+                'transfer_data' => [
+                    'amount' => $amount * 100,
+                    'destination' => 'acct_1N2cL7HC6nuER2HS'
+                ],
+            ]);
+
+            event(new Subscribed($subscription, $user));
+            return $subscription;
+        });
+    }
+}
---
+++ database/migrations/2023_09_30_091421_create_subscriptions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Plan;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->time('formation_duration'); // durée de formation qu'il veut acheter
+            $table->string('card_number'); // on enregistre seulement sa carte de crédit
+            $table->foreignIdFor(Plan::class);
+            $table->boolean('is_paid')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
