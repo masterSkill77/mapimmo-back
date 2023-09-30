fe3dfbd4f3e5bf966000a231fb128a4306d9fa1c
Dominique <tojonantenaina08@gmail.com>
2023-09-30 21:54:15 +0300

    fix: Update User

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,7 +4,7 @@ namespace App\Services;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
-use App\Http\Requests\UserUpdateRequest;
+
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
@@ -34,15 +34,15 @@ class UserService
         return ['user' => $user, 'token' => $token->plainTextToken];
     }
 
-    public function update(UserUpdateRequest $request) : User
+    public function update(RegisterRequest $request) : User
     {
+      
         $user = auth()->user();
         if($user){
-           $data = $request->toArray();
-           $data['password'] = Hash::make($data['password']);
-           $user = new User($data);
-           $user->save();
-           return $user;
+            $data = $request->toArray();
+             $data['password'] = Hash::make($data['password']);
+            $user->update($data);
+            return $user;
         }
         throw new NotFoundHttpException("User  not found");
     }
