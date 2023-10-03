5fc4db624e6b47ededb57f27db6e4510fb28bc52
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-10-03 22:35:17 +0300

    fix : remove view on sending email

--- app/Mail/SendConfirmationMail.php
+++ app/Mail/SendConfirmationMail.php
@@ -38,7 +38,7 @@ class SendConfirmationMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.mail.registermail',
+            view: 'mail.registermail',
         );
     }
 
