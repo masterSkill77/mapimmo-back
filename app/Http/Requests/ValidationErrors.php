5a491ea23f24555c77cfc23df1469b54619b80a4
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-10-04 21:34:32 +0300

    fix : return status bad request 400 on validation errors

--- app/Http/Requests/ValidationErrors.php
+++ app/Http/Requests/ValidationErrors.php
@@ -4,11 +4,12 @@ namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
 
 trait ValidationErrors
 {
     protected function failedValidation(Validator $validator)
     {
-        throw new HttpResponseException(response()->json(['errors' => $validator->errors()]));
+        throw new HttpResponseException(response()->json(['errors' => $validator->errors()], Response::HTTP_BAD_REQUEST));
     }
 }
