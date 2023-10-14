eb4ac75934e48b5f20c60f4a41b439fe017a38cc
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-10-14 23:31:41 +0300

    fix : belongsTo mais pas hasOne

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order extends Model
@@ -11,14 +12,17 @@ class Order extends Model
     use HasFactory;
 
     protected $fillable = [
-        'plans','total_amount', 'total_quantity','total_duration','user_id', 'num_invoice'
+        'plans', 'total_amount', 'total_quantity', 'total_duration', 'user_id', 'num_invoice'
     ];
 
     protected $casts = [
         'plans' => 'json'
     ];
-
-    public function user() :HasOne {
-        return $this->hasOne(User::class, 'id');
+    /**
+     * Belongs to mais pas HasOne
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
