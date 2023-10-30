f10606c21d0125c035f73cd9c5a35123da0b9125
Fabriola <fabriola@hightao-mg.com>
2023-10-30 07:33:50 +0300

    list formation

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -28,7 +28,7 @@ class RegisterRequest extends FormRequest
             'password' => 'required|string',
             'enterprise_name' => 'required|string',
             'lastname' => 'required|string',
-            'phone_number' => 'required|int',
+            'phone_number' => 'required|string',
             'card_number' => 'required|int',
             'card_month_expires' => 'required|int',
             'card_year_expires' => 'required|int',
