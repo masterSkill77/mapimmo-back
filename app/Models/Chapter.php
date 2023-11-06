c29066f982bbe024c13015ae026e91b8ad861b99
Nantenaina08 <tojonantenaina08@gmail.com>
2023-11-06 22:56:47 +0300

    fix: get Formation from Chapter

--- app/Models/Chapter.php
+++ app/Models/Chapter.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Chapter extends Model
 {
@@ -19,5 +21,10 @@ class Chapter extends Model
     public function lessons(): HasMany
     {
         return $this->hasMany(Lesson::class);
+    }
+
+    public function formation() : BelongsTo
+    {
+        return $this->belongsTo(Formation::class, 'formation_id');
     }
 }
