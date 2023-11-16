de66d0ba8057f35c450f8fba2a735a0e80c73086
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-11-16 10:45:23 +0300

    feat : update image formation

--- app/Http/Controllers/API/V1/FormationController.php
+++ app/Http/Controllers/API/V1/FormationController.php
@@ -83,7 +83,14 @@ class FormationController extends Controller
     }
     public function update(Request $request, $formationId): JsonResponse
     {
-        $formation = $this->formationService->update($request->all(), $formationId);
+        $data = $request->all();
+        if($request->file('photo'))
+        {
+                $filename =time() . '.' . $request->file('photo')->getClientOriginalExtension();
+                $path = $request->file('photo')->move(public_path('/storage'), $filename);
+                $data['photo'] = '/' . $filename;
+        }
+        $formation = $this->formationService->update($data, $formationId);
         if ($formation) {
             return response()->json($formation, 200);
         } else {
