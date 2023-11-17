a45ebd9a61c7e5d73d3a20671f34ab82fea118ae
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-11-17 21:07:50 +0300

    fix : update to array_diff only

--- app/Services/QuizzService.php
+++ app/Services/QuizzService.php
@@ -18,7 +18,7 @@ class QuizzService
                 $validated = true;
         }
         else {
-            $correctAnswer = array_diff_assoc(explode('|', $answer), explode('|', $question->correct_answer));
+            $correctAnswer = array_diff(explode('|', $answer), explode('|', $question->correct_answer));
             if (count($correctAnswer) == 0)
                 $validated = true;
         }
