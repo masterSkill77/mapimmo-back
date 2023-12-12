8223721638210633ce28daa12ec67a6c3b1759e3
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-12-12 17:37:12 +0300

    feat : add send mail for the contact page

---
+++ app/Services/SendMailService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+use App\Mail\ContactMail;
+use Illuminate\Mail\SentMessage;
+use Illuminate\Support\Facades\Mail;
+
+class SendMailService
+{
+    public function __construct()
+    {
+    }
+    public function sendMail(string $fullname, string $email, string $phoneNumber, string $subject, string $message): SentMessage | null
+    {
+        // formation@mapim-immo.fr
+        return Mail::to('clairmont.rajaonarison@gmail.com')->send(new ContactMail($fullname, $email, $phoneNumber, $subject, $message));
+    }
+}
---
+++ resources/views/mails/contact.blade.php
@@ -0,0 +1,25 @@
+<div>
+    <p>
+        Bonjour <b>MAPIM IMMO</b>, un message vient d'être posté par :
+        <ul style="list-style-type: none">
+            <li>
+                Nom et prénom : {{$fullname}}
+            </li>
+            <li>
+                Email : {{$email}}
+            </li>
+            <li>
+                Téléphone : {{$phoneNumber}}
+            </li>
+            <li>
+                Sujet : {{$subjectMail}}
+            </li>
+            <li>
+                Contenu du message :
+                <p>
+                    {{$message}}
+                </p>
+            </li>
+        </ul>
+    </p>
+</div>
